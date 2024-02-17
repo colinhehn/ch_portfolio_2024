36d2010fe09073884fa33f0866431927d1fc14bc
Colin Hehn <chehn@uiowa.edu>
2024-02-17 04:14:31 -0600

    Added cool tailwind button animation + made typing serif

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -33,9 +33,9 @@ const Main = () => {
             <div className="footer">
                 <div className="footer">
                     <a href="https://linktr.ee/colinhehn" target='_blank'>
-                        <button className="bg-black bg-opacity-50 text-white rounded-lg flex items-center justify-center text-xl p-4 fixed bottom-4 left-1/2 transform -translate-x-1/2">
-                            <img src="/lt_icon.webp" alt="Linktree Logo" className="mr-2 h-5 w-5" />
-                            See what I&apos;m up to!
+                        <button className="bg-black bg-opacity-50 text-white rounded-lg flex items-center justify-center text-l p-4 fixed bottom-4 left-1/2 transform -translate-x-1/2 overflow-hidden shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-orange-600 before:duration-500 before:ease-out hover:shadow-orange-600 hover:before:h-80 hover:before:w-80">
+                            <img src="/lt_icon.webp" alt="Linktree Logo" className="mr-2 h-5 w-5 relative" />
+                            <span className="text-transform none relative z-10">See what I&apos;m up to!</span>
                         </button>
                     </a>
                 </div>
