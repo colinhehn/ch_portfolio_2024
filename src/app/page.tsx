30730484245acbb7ed1254db3d0178fa476560a5
Colin Hehn <chehn@uiowa.edu>
2025-04-06 00:57:18 -0500

    Added typed and lots of styling, need font imports next!!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import "./globals.css";
+import "./page.css";
+import Typed_Title from "./typed_title";
 
 export default function Home() {
   return (
     <div>
       <main>
+        <Typed_Title />
         <p>I'm Colin, and here's a collection of my work.</p>
         <nav>
-          <a href="/about">About</a>
+          <a href="./about">About</a>
           <a href="/projects">Projects</a>
         </nav>
-        <img src="withthewater.jpg"></img>
-        <div className="social_button_nav">
+        <img src="withthewater.jpg" className="headshot_main"></img>
+        <div className="social_button_navigation">
           <a href="https://github.com/colinhehn" target="_blank">
-            <img src="github-brands.svg"></img>
+            <img src="github-brands.svg" className="social_button"></img>
           </a>
           <a href="https://www.linkedin.com/in/colinhehn/" target="_blank">
-            <img src="linkedin-brands.svg"></img>
+            <img src="linkedin-brands.svg" className="social_button"></img>
           </a>
         </div>
-      </main>
-      <footer>
         <p>&copy; 2025 Colin Hehn</p>
-      </footer>
+      </main>
     </div>
   );
 }
