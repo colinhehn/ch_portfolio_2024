1dae8f6eceecaecc3c9619ab2d3638b1546f13c6
Colin Hehn <chehn@uiowa.edu>
2025-05-25 19:03:47 -0500

    Nav is GONE! Taking single-page React dev to a new level. Let's let each platform do the talking.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import "./page.css";
 import Typed_Title from "./components/typed_title";
 import SocialNavigation from "./components/social_nav";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,19 +10,8 @@ export default function Home() {
       <main>
         <Typed_Title />
         <p className="main_subtitle">
-          {"I'm Colin, and here's a collection of my work."}
+          {"I'm Colin, a developer and musician from Chicago."}
         </p>
-        <nav>
-          <Link href="/aboutme">
-            <div className="nav_button">About Me</div>
-          </Link>
-          <Link href="/software">
-            <div className="nav_button">Software</div>
-          </Link>
-          <Link href="/music">
-            <div className="nav_button">Music</div>
-          </Link>
-        </nav>
         <img src="withthewater.jpg" className="headshot_main"></img>
         <SocialNavigation />
         <p>&copy; 2025 Colin Hehn</p>
